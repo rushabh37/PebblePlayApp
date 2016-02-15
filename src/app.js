0569d8b52a08cf54fc3b1293298c46e1f507aa03
rushabh37 <rushabh37@gmail.com>
2016-02-14 20:50:20 -0500

    Adding weather info for Seattle.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 
 // UI Module has windows and cards
 var UI = require('ui');
+var ajax = require('ajax');
+
+var APPID = '5d03929aca48a14cb15264b52293cb8f';
+var CITYNAME = 'Seattle';
+var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + 
+          CITYNAME
+          + '&appid=' + APPID;
 
 // Create a card with title and subtitle
 var card = new UI.Card({
@@ -10,3 +17,29 @@ var card = new UI.Card({
 
 // Display the Card
 card.show()
+
+// Make the request
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    console.log('Successfully fetched weather data!');
+    // Extract data
+    var location = data.name;
+    var temperature = Math.round(data.main.temp - 273.15) + 'C';
+
+     // Always upper-case first letter of description
+     var description = data.weather[0].description;
+     description = description.charAt(0).toUpperCase() + description.substring(1);
+     // Show to user
+     card.subtitle(location + ', ' + temperature);
+     card.body(description);
+  },
+  function(error) {
+    // Failure!
+    console.log('Failed fetching weather data: ' + error);
+  }
+);
